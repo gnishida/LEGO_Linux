be0065f119fad3ff697e86f45bbe633f127b063a
gnishida <gnishida@purdue.edu>
2018-01-31 14:18:32 -0500

    update

--- src/PlyWriter.cpp
+++ src/PlyWriter.cpp
@@ -2,17 +2,117 @@
 #include <fstream>
 
 namespace lego {
+	
+	namespace ply {
 
-	void write(char* filename, const std::vector<Building>& buildings) {
-		std::ofstream out(filename);
+		void write(char* filename, const std::vector<Building>& buildings) {
+			std::ofstream out(filename);
+			out << "ply" << std::endl;
+			out << "format binary_little_endian 1.0" << std::endl;
+			out << "element vertex 0" << std::endl;
+			out << "property double x" << std::endl;
+			out << "property double y" << std::endl;
+			out << "property double z" << std::endl;
+			out << "element face 0" << std::endl;
+			out << "property list uchar int vertex_indices" << std::endl;
+			out << "end_header" << std::endl;
 
-		// count the number of vertices and faces
-		std::vector<cv::Point3f> points;
-		for (int i = 0; i < buildings.size(); i++) {
-			//buildings[i].
+			std::vector<cv::Point3f> points;
+			std::vector<std::vector<int>> faces;
+			for (int i = 0; i < buildings.size(); i++) {
+				if (buildings[i].holes.size() == 0) {
+
+				}
+				else {
+
+				}
+			}
+
+			out.close();
 		}
 
-		out.close();
+		std::vector<std::vector<cv::Point2f>> tessellate(const std::vector<cv::Point2f>& points, const std::vector<std::vector<cv::Point2f>>& holes) {
+			std::vector<std::vector<cv::Point2f>> ans;
+
+			//Insert the polygons into a constrained triangulation
+			CDT cdt;
+			Polygon_2 polygon;
+			for (int j = 0; j < points.size(); j++) {
+				polygon.push_back(Point(points[j].x, points[j].y));
+			}
+			cdt.insert_constraint(polygon.vertices_begin(), polygon.vertices_end(), true);
+			for (int i = 0; i < holes.size(); i++) {
+				Polygon_2 polygon;
+				for (int j = 0; j < holes[i].size(); j++) {
+					polygon.push_back(Point(holes[i][j].x, holes[i][j].y));
+				}
+				cdt.insert_constraint(polygon.vertices_begin(), polygon.vertices_end(), true);
+			}
+
+			//Mark facets that are inside the domain bounded by the polygon
+			mark_domains(cdt);
+
+			for (CDT::Finite_faces_iterator fit = cdt.finite_faces_begin(); fit != cdt.finite_faces_end(); ++fit) {
+				if (fit->info().in_domain()) {
+					std::vector<cv::Point2f> polygon;
+					for (int i = 0; i < 3; i++) {
+						CDT::Vertex_handle vh = fit->vertex(i);
+						polygon.push_back(cv::Point2f(vh->point().x(), vh->point().y()));
+					}
+					ans.push_back(polygon);
+				}
+			}
+		}
+
+		void mark_domains(CDT& ct, CDT::Face_handle start, int index, std::list<CDT::Edge>& border) {
+			if (start->info().nesting_level != -1){
+				return;
+			}
+			std::list<CDT::Face_handle> queue;
+			queue.push_back(start);
+			while (!queue.empty()){
+				CDT::Face_handle fh = queue.front();
+				queue.pop_front();
+				if (fh->info().nesting_level == -1){
+					fh->info().nesting_level = index;
+					for (int i = 0; i < 3; i++){
+						CDT::Edge e(fh, i);
+						CDT::Face_handle n = fh->neighbor(i);
+						if (n->info().nesting_level == -1){
+							if (ct.is_constrained(e)) {
+								border.push_back(e);
+
+
+							}
+							else queue.push_back(n);
+						}
+					}
+				}
+			}
+		}
+
+		//explore set of facets connected with non constrained edges,
+		//and attribute to each such set a nesting level.
+		//We start from facets incident to the infinite vertex, with a nesting
+		//level of 0. Then we recursively consider the non-explored facets incident 
+		//to constrained edges bounding the former set and increase the nesting level by 1.
+		//Facets in the domain are those with an odd nesting level.
+		void mark_domains(CDT& cdt) {
+			for (CDT::All_faces_iterator it = cdt.all_faces_begin(); it != cdt.all_faces_end(); ++it){
+				it->info().nesting_level = -1;
+			}
+			std::list<CDT::Edge> border;
+			mark_domains(cdt, cdt.infinite_face(), 0, border);
+			while (!border.empty()){
+				CDT::Edge e = border.front();
+				border.pop_front();
+				CDT::Face_handle n = e.first->neighbor(e.second);
+				if (n->info().nesting_level == -1){
+					mark_domains(cdt, n, e.first->info().nesting_level + 1, border);
+				}
+			}
+		}
+
 	}
 
 }
--- src/PlyWriter.h
+++ src/PlyWriter.h
@@ -2,10 +2,55 @@
 
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Partition_traits_2.h>
+#include <CGAL/partition_2.h>
+#include <CGAL/point_generators_2.h>
+#include <CGAL/random_polygon_2.h>
+#include <CGAL/Polygon_2.h>
+#include <CGAL/create_offset_polygons_2.h>
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <CGAL/Polygon_2.h>
+#include <CGAL/random_polygon_2.h>
+#include <CGAL/Polygon_2.h>
 #include "Building.h"
 
 namespace lego {
 
-	void write(char* filename, const std::vector<Building>& buildings);
+	namespace ply {
 
+		// The following definitions are for triangulation only.
+		struct FaceInfo {
+			FaceInfo() {}
+			int nesting_level;
+			bool in_domain(){
+				return nesting_level % 2 == 1;
+			}
+		};
+
+		typedef CGAL::Exact_predicates_inexact_constructions_kernel       K;
+		typedef CGAL::Triangulation_vertex_base_2<K>                      Vb;
+		typedef CGAL::Triangulation_face_base_with_info_2<FaceInfo, K>    Fbb;
+		typedef CGAL::Constrained_triangulation_face_base_2<K, Fbb>        Fb;
+		typedef CGAL::Triangulation_data_structure_2<Vb, Fb>               TDS;
+		typedef CGAL::Exact_predicates_tag                                Itag;
+		typedef CGAL::Constrained_Delaunay_triangulation_2<K, TDS, Itag>  CDT;
+		typedef CDT::Point                                                Point;
+		typedef CGAL::Partition_traits_2<K>                         Traits;
+		typedef Traits::Polygon_2                                   Polygon_2;
+		typedef Traits::Point_2                                     Point_2;
+		typedef Polygon_2::Vertex_iterator                          Vertex_iterator;
+		typedef std::list<Polygon_2>                                Polygon_list;
+		typedef CGAL::Creator_uniform_2<int, Point_2>               Creator;
+		typedef CGAL::Random_points_in_square_2< Point_2, Creator > Point_generator;
+		typedef boost::shared_ptr<Polygon_2>						PolygonPtr;
+
+		void write(char* filename, const std::vector<Building>& buildings);
+		
+		std::vector<std::vector<cv::Point2f>> tessellate(const std::vector<cv::Point2f>& points, const std::vector<std::vector<cv::Point2f>>& holes);
+		void mark_domains(CDT& ct, CDT::Face_handle start, int index, std::list<CDT::Edge>& border);
+		void mark_domains(CDT& cdt);
+
+	}
 }
