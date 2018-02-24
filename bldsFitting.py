9c69ab5ee913e6d35b644ebfdab44decea6e7c33
gnishida <gnishida@purdue.edu>
2018-02-24 12:11:37 -0500

    update

--- bldsFitting.py
+++ bldsFitting.py
@@ -7,10 +7,11 @@ def main(args=None):
     
     parser = argparse.ArgumentParser(description='Building Modeling from vdb')
     
-    parser.add_argument('input_vdb', type=str, help='input vdb filename')
-    parser.add_argument('input_binary', type=str, help='input binary filename')
-    parser.add_argument('output', type=str, help='output ply filename')
-    parser.add_argument('-t', '--threshold', type=float, help='threshold')
+    parser.add_argument('input_vdb', type=str, requried=True, help='input vdb filename')
+    parser.add_argument('input_binary', type=str, requried=True, help='input binary filename')
+    parser.add_argument('output', type=str, requried=True, help='output ply filename')
+    #parser.add_argument('-t', '--threshold', type=float, help='threshold')
+    parser.add_argument('-w', '--weight', type=float, requried=True, help='The relative weight of accuracy term to the simplicity term [0, 1].')
     args = parser.parse_args(args)
 
     # slices_folder name is a temporary fold to store the slice images
@@ -21,18 +22,18 @@ def main(args=None):
     cmd = " ".join(["mkdir", slices_folder])
     print(cmd)
     os.system(cmd)
-    args.threshold = 0.5
-    if args.threshold is None:
-        cmd = " ".join(["lego", "vdb.horzSlice", args.input_vdb, "--name", slices_folder + "/slice_%03d.png"])
-        print(cmd)
-        os.system(cmd)
-    else:
-        cmd = " ".join(["lego", "vdb.horzSlice", args.input_vdb, "--name", slices_folder + "/slice_%03d.png",
-                        "--threshold", str(args.threshold)])
-        print(cmd)
-        os.system(cmd)
+    #args.threshold = 0.5
+    #if args.threshold is None:
+    #    cmd = " ".join(["lego", "vdb.horzSlice", args.input_vdb, "--name", slices_folder + "/slice_%03d.png"])
+    #    print(cmd)
+    #    os.system(cmd)
+    #else:
+    cmd = " ".join(["lego", "vdb.horzSlice", args.input_vdb, "--name", slices_folder + "/slice_%03d.png",
+                        "--threshold", "0.5"])
+    print(cmd)
+    os.system(cmd)
 
-    cmd = " ".join(["./" + args.input_binary, slices_folder + "/slice_001.png", args.output])
+    cmd = " ".join(["./" + args.input_binary, slices_folder + "/slice_001.png", str(arg.weight), args.output])
     #cmd = " ".join(["./" + args.input_binary, slices_folder + "/slice_000.png"])
     print(cmd)
     os.system(cmd)
